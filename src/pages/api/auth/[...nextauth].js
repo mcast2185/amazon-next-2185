889d6b7ad07c12395b38ccaee04871696ad6c076
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2024-02-21 11:37:44 -0700

    env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,24 +1,15 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from "next-auth/providers/apple";
 
 
-export const authOptions = {
+const googleId = String(process.env.GOOGLE_ID);
+const googleSecret = String(process.env.GOOGLE_SECRET);
+export default NextAuth ({
 
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: googleId,
+      clientSecret: googleSecret,
     }),
-    // AppleProvider({
-    //   clientId: process.env.APPLE_ID,
-    //   clientSecret: process.env.APPLE_SECRET,
-    // }),
   ],
-}
-export default NextAuth(authOptions)
+})
